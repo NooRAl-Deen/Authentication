dc6a085ad50d69c8e04908a7e6705e021591116c
RajabM1 <rajab1.masri@gmail.com>
2022-03-04 15:07:29 +0200

    change password validation

---
+++ public/js/enterPass.js
@@ -0,0 +1,8 @@
+function matchPassword() {
+    let pw1 = document.getElementById("pas1").value;
+    let pw2 = document.getElementById("pas2").value;
+    if(pw1 !== pw2)
+    {	
+        alert("Passwords did not match");
+    }
+  }
